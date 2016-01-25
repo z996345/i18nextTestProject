9228d3115be102b2ebb3fcb12bcc055c1e2d45d0
z996345 <z996345@hotmail.com>
2016-01-25 12:00:41 +0800

    Finish i18/next Project Test (using node.js and i18next-1.11.2 version), i18next 2.x up version is  too hard, so not try it.

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app     = express();
+// app.use(express.static(__dirname + '/View'));
+//Store all HTML files in view folder.
+// app.use(express.static(__dirname + '/Script'));
+//Store all JS and CSS in Scripts folder.
+app.use(express.static(__dirname));
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
+app.listen(3080);
+
+console.log("Running at Port 3080");
